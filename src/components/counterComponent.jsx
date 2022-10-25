b106db801c0557c6956f604c8d0808de9a84da4b
leosanchez <slash902daniel@gmail.com>
2022-10-25 10:18:35 -0500

    Render classes dinamycally

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -11,12 +11,19 @@ class Counter extends Component {
   }
 
   render() {
+
     return (
       <React.Fragment>
-        <span style={this.badgeStyles} className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button style={ {fontSize: 20 }} className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
