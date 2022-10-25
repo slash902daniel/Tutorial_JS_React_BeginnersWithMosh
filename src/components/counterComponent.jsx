f44f5222c6c4d96614bcf1c73b6e9f0961e79c59
leosanchez <slash902daniel@gmail.com>
2022-10-25 14:16:31 -0500

    Composing Components AND passing data

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {  
-    count: 0,
+    value: 0,
     tags: ["tag1", "tag2", "tag3"]
   }
 
@@ -23,22 +23,23 @@ class Counter extends Component {
   }
 
   handleIncrement(){
-    this.setState({count: this.state.count + 1});
+    this.setState({value: this.state.value + 1});
   }
 
   handleIncrement_V_Experimental = product => {
     console.log("Increment Clicked:product:  ", product);
     console.log("Increment Clicked this: ", this);
     console.log("Increment Clicked2 props: ", this.props);
-    this.setState({count: this.state.count + 1});
+    console.log("Increment Clicked2 props.value: ", this.props.value);
+    this.setState({value: this.state.value + 1});
   }
 
   render() {
-
+    console.log("render props: ", this.props);
     return (
       <React.Fragment>
         <div>
-          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
           <button onClick={ () => this.handleIncrement_V_Experimental({ id: 1 }) } className="btn btn-secondary btn-sm">Increment</button>
           {this.state.tags===0 && 'Please create a new tag!'}
           {this.renderTags()}  
@@ -49,13 +50,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.state.value === 0) ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count
+  formatValue() {
+    const { value } = this.state;
+    return value === 0 ? <h1>Zero</h1> : value
    }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counterComponent';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>
 );
 
