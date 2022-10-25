4312628b8a5031a1b926caedf3b81c44b8b209d9
leosanchez <slash902daniel@gmail.com>
2022-10-25 10:45:00 -0500

    Handling events

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -17,13 +17,18 @@ class Counter extends Component {
     return <ul> {this.state.tags.map(tag => <li>{tag}</li>)} </ul>;
   }
 
+  handleIncrement(){
+    console.log("Increment Clicked");
+
+  }
+
   render() {
 
     return (
       <React.Fragment>
         <div>
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button className="btn btn-secondary btn-sm">Increment</button>
+          <button onClick={this.handleIncrement()} className="btn btn-secondary btn-sm">Increment</button>
           {this.state.tags===0 && 'Please create a new tag!'}
           {this.renderTags()}  
         </div>
