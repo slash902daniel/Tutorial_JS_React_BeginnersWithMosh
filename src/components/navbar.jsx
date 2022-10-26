60fddc029f5779dce513b70b56afa4e17b96302a
leosanchez <slash902daniel@gmail.com>
2022-10-26 10:37:42 -0500

    Stateless Functional Components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,25 @@
 import React, {Component} from "react";
 
-class NavBar extends Component {
-    render() { 
-        return (
-            <nav className="navbar bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        Navbar
-                        <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-                    </a>
-                </div>
-            </nav>
-        );
-    }
+//stateless Functional Component, as it does only receive from props.
+//so rather than a class we will use a function 
+//ofter used by devs when simple is needed
+//note that we need to add props as arg, and remove "this"
+
+//Further we can remove the props. by destructuring the call to extract the variable used ({totalCounters})
+
+const NavBar = ({totalCounters}) => {
+    return (
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    Navbar
+                    <span className="badge badge-pill badge-secondary">
+                        {totalCounters}
+                    </span>
+                </a>
+            </div>
+        </nav>
+    );
 }
 
 export default NavBar;
