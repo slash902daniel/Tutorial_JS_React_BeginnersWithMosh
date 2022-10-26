5e60d2225cb273db9b59ffb10326bdcc481a7dfc
leosanchez <slash902daniel@gmail.com>
2022-10-26 10:27:29 -0500

    Lifting State Up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import logo from './logo.svg';
+import React, { Component} from "react";
+//import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar';
 import Counters from './components/counters';
-import React from 'react';
+class App extends Component {
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+  state = {
+    counters: [
+        {id: 1, value: 0},
+        {id: 2, value: 4},
+        {id: 3, value: 0},
+        {id: 4, value: 0}
+    ]
+  }
   
+  handleIncrement = (counter) => {
+    console.log("handleIncrement - Event Handler Called", counter);
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({counters});
+  };
+  
+  handleDelete = (counterId) => {
+    console.log("handleDelete - Event Handler Called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+  };
+  
+  handleReset = () => {
+    console.log("handleReset - Event Handler Reset called");
+    const counters = this.state.counters.map(c => {
+        c.value = 0;
+        return c;
+    })
+  
+    this.setState({counters});
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
+        <main className="container">
+          <Counters 
+          counters={this.state.counters}
+          onReset={this.handleReset} 
+          onIncrement={this.handleIncrement} 
+          onDelete={this.handleDelete}
+        />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
